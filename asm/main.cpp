180d8bbe3d7317bc51926c8c4aeb3c7de1766f89
Michal Flaška <109202734+pilot2254@users.noreply.github.com>
2026-08-09 09:41:20 +0200

    add max and sub to asm

--- asm/main.cpp
+++ asm/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 extern "C" long long asm_add(long long a, long long b);
+extern "C" long long asm_sub(long long a, long long b);
+
+extern "C" long long asm_max(long long a, long long b);
 
 int main() {
         long long result = asm_add(5, 10);
--- asm/piss.asm
+++ asm/piss.asm
@@ -1,8 +1,12 @@
 .code
 
-; assuming that our func call is `asm_add(5, 10);`
+
+;
+
 
 asm_add PROC
+    ; assuming that our func call is `asm_add(5, 10);`
+
     ; when this function is called rcx already holds the 1st argument (5)
     ; and rdx already holds the 2nd argument (10)
     ; just a fixed rule the cpu/compiler follows (not something we write)   
@@ -16,6 +20,40 @@ asm_add PROC
     ret             ; return to main()
                     ; whatever value is in rax right now becomes the function's return value
 asm_add ENDP
+
+
+;
+
+
+asm_sub PROC
+
+    mov rax, rcx
+    sub rax, rdx
+    ret
+
+asm_sub ENDP
+
+
+;
+
+
+asm_max PROC
+
+    cmp rcx, rdx
+    jg greater
+
+    mov rax, rdx
+    ret
+
+    greater:
+        mov rax, rcx
+        ret
+
+asm_max ENDP
+
+
+;
+
 
 END
 
