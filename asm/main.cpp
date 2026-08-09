637621228666ef411970e682b085680f3c44dae0
Michal Flaška <109202734+pilot2254@users.noreply.github.com>
2026-08-09 12:18:45 +0200

    taking a break

--- asm/main.cpp
+++ asm/main.cpp
@@ -5,9 +5,35 @@ extern "C" long long asm_sub(long long a, long long b);
 
 extern "C" long long asm_max(long long a, long long b);
 
+extern "C" long long asm_sum(long long n);
+
+extern "C" long long asm_factorial(long long n);
+
+extern "C" long long asm_array_sum(long long array[], long long length);
+extern "C" long long asm_array_double(long long array[], long long length);
+extern "C" long long asm_array_max(long long array[], long long length);
+
+extern "C" ling ling asm_array_reverse(long long array[], long long length);
+
 int main() {
-        long long result = asm_add(5, 10);
-        std::cout << result;
+        
+
+        std::cout << asm_add(5, 10)     << '\n';
+        std::cout << asm_sub(5, 10)     << '\n';
+
+        std::cout << asm_max(5, 10)     << '\n';
+        std::cout << asm_sum(5)         << '\n';
+        std::cout << asm_factorial(5)   << '\n';
+
+        long long arr[] = {5, 2, 3, 4, 1};
+        std::cout << asm_array_sum(arr, (long long)sizeof(arr)/sizeof(arr[0])) << '\n';
+
+        asm_array_double(arr, (long long)sizeof(arr)/sizeof(arr[0]));
+        for (long long x : arr) std::cout << x << ' ';
+        std::cout << '\n';
+
+        std::cout << asm_array_max(arr, (long long)sizeof(arr)/sizeof(arr[0])) << '\n';
+
         std::cin.get();
         return 0;
 }
--- asm/piss.asm
+++ asm/piss.asm
@@ -45,9 +45,9 @@ asm_max PROC
     mov rax, rdx
     ret
 
-    greater:
-        mov rax, rcx
-        ret
+greater:
+    mov rax, rcx
+    ret
 
 asm_max ENDP
 
@@ -55,6 +55,136 @@ asm_max ENDP
 ;
 
 
+asm_sum PROC
+
+    mov rax, 0
+
+loop_start:
+    cmp rcx, 0
+    jz finish
+    add rax, rcx
+    dec rcx
+    jmp loop_start
+
+finish:
+    ret
+
+asm_sum ENDP
+
+
+;
+
+
+asm_factorial PROC
+
+    mov rax, 1
+
+loop_start:
+    cmp rcx, 0
+    jz finish
+
+    imul rax, rcx
+    dec rcx
+
+    jmp loop_start
+
+
+finish:
+    ret
+    
+asm_factorial ENDP
+
+
+;
+
+
+asm_array_sum PROC
+
+    mov rax, 0
+    mov rsi, 0
+
+loop_start:
+    cmp rsi, rdx
+    jz finish
+
+    add rax, [rcx + rsi*8]
+    inc rsi
+
+    jmp loop_start
+
+finish:
+    ret
+
+
+asm_array_sum ENDP
+
+
+;
+
+
+asm_array_double PROC
+
+    mov rsi, 0
+
+loop_start:
+    cmp rsi, rdx
+    jz  finish
+
+    mov rax, [rcx + rsi*8]   ; load arr[i]
+    add rax, rax             ; double it
+    mov [rcx + rsi*8], rax   ; store back into arr[i]
+    inc rsi
+
+    jmp loop_start
+
+finish:
+    ret
+asm_array_double ENDP
+
+
+;
+
+
+asm_array_max PROC
+
+    mov rax, [rcx]
+    mov rsi, 0
+
+loop_start:
+    cmp rsi, rdx
+    jz finish
+
+    cmp [rcx + rsi*8], rax
+    jg greater
+    inc rsi
+    jmp loop_start
+
+greater:
+    mov rax, [rcx + rsi*8]
+    inc rsi
+    jmp loop_start
+
+finish:
+    ret
+
+asm_array_max ENDP
+
+
+;
+
+
+asm_array_reverse PROC
+
+    mov rax, 0
+
+loop_start:
+    ; todo
+
+finish:
+    ret
+
+asm_array_reverse ENDP
+
 END
 
-; came back from a long fucking vacation and i forgot everything. im sorry assembly, i still love you
+; came back home from a long fucking vacation and i forgot everything. im sorry assembly, i still love you
