798c3f254d6c8b2e7e23beb5ba3829f6c6133993
Michal Flaška <109202734+pilot2254@users.noreply.github.com>
2026-08-24 13:05:55 +0200

    update run forever code

---
+++ run-forever/main.cpp
@@ -0,0 +1,11 @@
+#define WIN32_LEAN_AND_MEAN // i hate windows bloat :3
+#include <Windows.h>
+#include <iostream>
+
+int main() {
+        std::cout << "alr buddy i have no idea why but here you go..." << '\n';
+        while (true) {Sleep(INFINITE); }
+        return 0;
+}
+
+// use -static flag
